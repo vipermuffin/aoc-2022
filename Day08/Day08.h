f4332912adfd0e71911346649f1b984f04e19d47
Viper Muffin <vipermuffin@gmail.com>
2022-12-08 19:07:34 -0600

    Day 08 Initial Solution

---
+++ Day08/Day08.h
@@ -0,0 +1,17 @@
+//
+//  Advent of Code 2022 Solutions: Day 8
+//
+//  https://adventofcode.com/2022
+//
+//  Created by vipermuffin on 12/07/2022.
+//  Copyright © 2022 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay08 {
+//Function Definitions
+int32_t countVisibleTrees(const std::vector<std::string>& grid);
+int64_t findHighestScenicVal(const std::vector<std::string>& grid);
+}
