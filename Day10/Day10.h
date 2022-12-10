3356ed74d3043859183d2423332f53b06a7f5464
vipermuffin <vipermuffin@gmail.com>
2022-12-10 11:55:53 -0600

    Day 10 Solution (#10)

---
+++ Day10/Day10.h
@@ -0,0 +1,17 @@
+//
+//  Advent of Code 2022 Solutions: Day 10
+//
+//  https://adventofcode.com/2022
+//
+//  Created by vipermuffin on 12/09/2022.
+//  Copyright © 2022 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay10 {
+//Function Definitions
+    std::vector<int64_t> runProgram(const std::vector<std::string>&, int64_t startVal);
+    std::vector<std::string> runProgram2(const std::vector<std::string>&, int64_t startVal);
+}
