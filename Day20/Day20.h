a8c47551c5b84995c4d53bec9209fa7f48a45fd0
Viper Muffin <vipermuffin@gmail.com>
2022-12-19 23:37:32 -0600

    Day 20 Init

---
+++ Day20/Day20.h
@@ -0,0 +1,16 @@
+//
+//  Advent of Code 2022 Solutions: Day 20
+//
+//  https://adventofcode.com/2022
+//
+//  Created by vipermuffin on 12/19/2022.
+//  Copyright © 2022 vipermuffin. All rights reserved.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+namespace AocDay20 {
+//Function Definitions
+
+}
